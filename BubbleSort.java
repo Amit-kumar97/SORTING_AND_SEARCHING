a56c41548082e0d85917d1f5f84cf7274781a4a4
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-03-14 22:32:09 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -52,6 +52,38 @@ public class BubbleSort {
             System.out.print(s+" ");
         }
     }
+    
+    
+//    complexity = O(n)
+    
+//     public static void bubbleSort(int arr[], int n)
+//    {
+//        if (n == 1)                     //passes are done
+//        {
+//            return;
+//        }
+//
+//        for (int i=0; i<n-1; i++)       //iteration through unsorted elements
+//        {
+//            if (arr[i] > arr[i+1])      //check if the elements are in order
+//            {                           //if not, swap them
+//                int p = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = p;
+//            }
+//        }
+//
+//        bubbleSort(arr, n-1);           //one pass done, proceed to the next
+//    }
+//    public static void display(int arr[])                 //display the array
+//    {
+//        for (int i=0; i<arr.length; ++i)
+//        {
+//            System.out.print(arr[i]+" ");
+//        }
+//    }
+    
+
     //--------main method
     public static void main(String args[])
     {
